bdae7cae160f74c105842c362f322a467c8ca081
benard <benardboiwo703@gmail.com>
2024-04-13 16:38:14 +0300

    inialiation

--- challenge -3.js
+++ challenge -3.js
@@ -1,16 +1,35 @@
-function calculateNetsalary(){
-    let basicSalary = ("basic-salary").value
-    let benefits = ("benefits").value
-    //calcutate payee (i.e. Tax)
-    let payee = math.round((basicSalary + benefits) * 0.5);
-    
-    //calculate NHIF deductions
-    let NHIFDeductions = math.round(basicSalary * 0.25);
+function calculateNetSalary(basicSalary,benefits){
+       //canstants
+    const TAX_RATE = 0.15; // 15% tax rate
+    const NHIF_RATE = 0.025; //2.5% NHIF rate
+    const NSSF_RATE = 0.06; //6% NSSF rate
+    //calculate gross salary
+    const grossSalary = basicSalary + benefits;
+     //calculate tax (payee)
+    const tax = grossSalary * TAX_RATE;
+     //calculate NHIF deductions
+    const nhifDeductions = grossSalary * NHIF_RATE;
+     //calculate NSSF deductions
+    const nssfDeduction = grossSalary * NSSF_RATE
+     //calculate net salary
+    const netsalary = grossSalary - tax -nhifDeductions-nssfDeduction 
+    // Return results
+    return{
+        grossSalary: grossSalary,
+        tax: tax,
+        nhifDeductions:nhifDeductions,
+        nssfDeduction:nssfDeduction,
+        netsalary:netsalary
 
-    //calculate NSSF deductions
-    let NSSFDeductions = maths.round(basicSalary * 0.8);
-    // calculate gross salary
-    let grossSalary = maths.round(basicSalary+benefits);
-    //calculate net salary
-    let netSalary = gross-payee-NHIFDeductions-NSSFDeductions;
+    }; 
 }
+//Example usage
+const basicSalary = 20000; // Example basic salary
+const benefits = 40000; // Example benefits
+
+const salaryDetails = calculateNetSalary(basicSalary,benefits)
+
+console.log("gross salary:", salaryDetails.grossSalary);
+console.log("Tax (PAYE):", salaryDetails.tax);
+console.log("NHIF Deduction:",salaryDetails.nhifDeductions);
+console.log("net salary:",salaryDetails.netsalary);
--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const a=-5;
 
-if (a===5){
-    console.log("equal")
-}
-else if (a>5){
-    console.log("greater")
-}
+
